bda3f7c539be7f506ab3c1b599d01e1eec80669d
quanta2015 <liyangtom@163.com>
2022-04-27 08:54:34 +0800

    init project

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+// $(init)
+
+// const init =()=> {
+//   $("body").on('click','.m-btn', hideBox)
+// }
+
+
+// const hideBox =()=> {
+//   $(".box").hide(1000)
+// }
+
+const $ =(e)=> { return document.querySelector(e) }
+const on =(e,cb)=> { $(e).addEventListener('click',cb) }
+const f = ()=>{ $('.box').classList.add('hide') }
+$('.m-btn').on('.box',f)
